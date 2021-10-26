c8b27d27c7071371680522a5a713e71d62077844
MaximilianoVillalba <maximiliano.villalba@est.fi.uncoma.edu.ar>
2021-10-26 20:23:28 -0300

    Simple modificadion de boton

--- resources/js/views/Informes/Informes.vue
+++ resources/js/views/Informes/Informes.vue
@@ -17,7 +17,9 @@
           </thead>
           <tbody>
             <tr
-              v-for="report in reports" :v-bind="report.report.report_id" v-if="report.state_id == etapa_id"
+              v-for="report in reports"
+              :v-bind="report.report.report_id"
+              v-if="report.state_id == etapa_id"
             >
               <td>{{ report.report.report_id }}</td>
               <td>{{ report.report.created }}</td>
@@ -29,7 +31,9 @@
                 <router-link
                   :to="{ path: `/detalleInforme/` + report.report.report_id }"
                   idInforme="idInforme"
-                  >+</router-link
+                  ><button class="btn btn-outline-info btn-sm">
+                    +
+                  </button></router-link
                 >
               </td>
             </tr>
@@ -42,52 +46,52 @@
 <script>
 import DetalleInforme from "../Informes/DetalleInforme.vue";
 export default {
-    components: {
-        DetalleInforme,
-    },
-    data() {
-        return {
-        reports: [],
-        nombre_etapa: "",
-        etapa_id: null,
-        };
-    },
-    mounted() {
-        this.$emit("nombreHijo", this.nombre);
-        this.currentRoute = this.$router.currentRoute.name;
+  components: {
+    DetalleInforme,
+  },
+  data() {
+    return {
+      reports: [],
+      nombre_etapa: "",
+      etapa_id: null,
+    };
+  },
+  mounted() {
+    this.$emit("nombreHijo", this.nombre);
+    this.currentRoute = this.$router.currentRoute.name;
 
-        this.getPosts(this.$route.query);
-    },
+    this.getPosts(this.$route.query);
+  },
   methods: {
     getPosts(query) {
-        if (query.sort !== "undefined" && query.direction) {
-            this.defaultClass[query.sort] = query.direction;
-        }
+      if (query.sort !== "undefined" && query.direction) {
+        this.defaultClass[query.sort] = query.direction;
+      }
 
-        axios
-            .get("api/informeempleadoestados", { params: query })
-            .then((response) => {
-                this.reports = response.data.reports;
-                console.log("lee aca maxiiii",this.reports);
-                this.queryParams = response.data.query;
-            })
-            .catch((error) => {
-            console.log("Error: " + error);
-            });
+      axios
+        .get("api/informeempleadoestados", { params: query })
+        .then((response) => {
+          this.reports = response.data.reports;
+          console.log("lee aca maxiiii", this.reports);
+          this.queryParams = response.data.query;
+        })
+        .catch((error) => {
+          console.log("Error: " + error);
+        });
     },
     //**Este metodo se ejecuta justo antes de cargar la vista , se cargan todos los datos pero todavia no se muestra la vista*/
-    },
-    created() {
-        if (!this.$session.exists()) {
-            this.nombre_etapa = "asdasd";
-            this.$router.push("/login");
-            console.log("saaale", nombre_etapa);
-        }
-        if (this.$session.exists()) {
-            this.nombre_etapa = this.$session.get("nombre_etapa");
-            this.etapa_id = this.$session.get("etapa_id");
-        }
-    },
+  },
+  created() {
+    if (!this.$session.exists()) {
+      this.nombre_etapa = "asdasd";
+      this.$router.push("/login");
+      console.log("saaale", nombre_etapa);
+    }
+    if (this.$session.exists()) {
+      this.nombre_etapa = this.$session.get("nombre_etapa");
+      this.etapa_id = this.$session.get("etapa_id");
+    }
+  },
 };
 </script>
 
