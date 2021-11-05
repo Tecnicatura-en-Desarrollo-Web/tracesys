1495d8fb1322a191cc4c2cfe9a0645e73f89fdb8
jonathanvictorrios <yonamixlfr@gmail.com>
2021-11-05 20:34:57 -0300

    se agregaron estilos y se corrigieron problemas con sidebar

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="page-wrapper chiller-theme toggled">
-    <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-      <i class="fas fa-bars"></i>
-    </a>
-    <nav id="sidebar" class="sidebar-wrapper">
-      <div class="sidebar-content">
-        <div class="sidebar-header">
-          <div class="user-pic">
-            <img
-              class="img-responsive img-rounded"
-              src="../../img/NicePng_usuario-png_2022264.png"
-              alt="User picture"
-            />
-          </div>
-          <div class="user-info">
-            <span class="user-name">
-              <strong>{{ nombreUsuario }}</strong>
-            </span>
-            <span class="user-role">{{ sector }}</span>
-          </div>
+    <div class="page-wrapper chiller-theme toggled">
+
+        <!-- <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+        <i class="fas fa-bars">GHFGHFGHFGH</i>
+        </a> -->
+        <nav id="sidebar" class="sidebar-wrapper">
+
+        <div class="sidebar-content">
+            <div class="sidebar-header">
+            <div class="user-pic">
+                <img
+                class="img-responsive img-rounded"
+                src="../../img/NicePng_usuario-png_2022264.png"
+                alt="User picture"
+                />
+            </div>
+            <div class="user-info">
+                <span class="user-name">
+                <strong>{{ nombreUsuario }}</strong>
+                </span>
+                <span class="user-role">{{ sector }}</span>
+            </div>
+            </div>
+            <div class="sidebar-menu">
+            <ul>
+                <li class="header-menu">
+                <router-link to="/home" class="nav-link"
+                    ><b-icon-shop class="icono" style="width: 25px; height: 25px;"></b-icon-shop>Home</router-link>
+                </li>
+                <li class="header-menu">
+                <router-link to="/reports"
+                    ><b-icon-file-text class="icono" style="width: 25px; height: 25px;"></b-icon-file-text>Informes</router-link>
+                </li>
+                <li class="header-menu">
+                <router-link to="/informes/crear"
+                    ><b-icon-journal-arrow-up class="icono" style="width: 25px; height: 25px;"></b-icon-journal-arrow-up>Cargar informe</router-link>
+                </li>
+                <li class="header-menu">
+                <router-link to="/sector/crear"
+                    ><b-icon-bookmark class="icono" style="width: 25px; height: 25px;"></b-icon-bookmark>Registrar sector</router-link>
+                </li>
+                <li class="header-menu">
+                <router-link to="/sector/ver"
+                    ><b-icon-bookmark class="icono" style="width: 25px; height: 25px;"></b-icon-bookmark>Ver sectores</router-link>
+                </li>
+                <li class="header-menu">
+                <router-link to="/suggestion/crear"
+                    >Crear sugerencia<b-icon-bookmark></b-icon-bookmark
+                ></router-link>
+                </li>
+            </ul>
+            </div>
         </div>
-        <div class="sidebar-menu">
-          <ul>
-            <li class="header-menu">
-              <router-link to="/home" class="nav-link"
-                ><b-icon-shop class="icono" style="width: 25px; height: 25px;"></b-icon-shop>Home</router-link>
-            </li>
-            <li class="header-menu">
-              <router-link to="/reports"
-                ><b-icon-file-text class="icono" style="width: 25px; height: 25px;"></b-icon-file-text>Informes</router-link>
-            </li>
-            <li class="header-menu">
-              <router-link to="/informes/crear"
-                ><b-icon-journal-arrow-up class="icono" style="width: 25px; height: 25px;"></b-icon-journal-arrow-up>Cargar informe</router-link>
-            </li>
-            <li class="header-menu">
-              <router-link to="/sector/crear"
-                ><b-icon-bookmark class="icono" style="width: 25px; height: 25px;"></b-icon-bookmark>Registrar sector</router-link>
-            </li>
-            <li class="header-menu">
-              <router-link to="/sector/ver"
-                ><b-icon-bookmark class="icono" style="width: 25px; height: 25px;"></b-icon-bookmark>Ver sectores</router-link>
-            </li>
-            <li class="header-menu">
-              <router-link to="/suggestion/crear"
-                >Crear sugerencia<b-icon-bookmark></b-icon-bookmark
-              ></router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <!--       <span class="user-name">
-        <strong>TraceSys Web</strong>
-      </span> -->
-    </nav>
-  </div>
+        <!--       <span class="user-name">
+            <strong>TraceSys Web</strong>
+        </span> -->
+        </nav>
+    </div>
   <!--   <div class="page-wrapper chiller-theme toggled">
     <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
       <i class="fas fa-bars"></i>
@@ -118,7 +120,7 @@ export default {
     //         })
     //         .catch((error) => {
     //         console.log("Error: " + error);
-    //         });
+    //         });s
   },
 };
 </script>
@@ -127,12 +129,17 @@ export default {
         margin:5px;
         border-radius: 5px;
         width: 250px;
+
     }
     .chiller-theme .sidebar-wrapper .sidebar-menu ul li a:hover{
-        background-color: rgb(88, 88, 88);
+        background-color: rgba(233, 233, 233, 0.116);
+
     }
     .chiller-theme .sidebar-wrapper .sidebar-menu ul li a:focus{
                 background-color: rgb(233, 233, 233);
+                color: #212121;
+                display: block;
+
 
     }
     .icono{
@@ -140,4 +147,11 @@ export default {
         margin-bottom: 10px;
         font-weight: normal;
     }
+    .focusedInput {
+        border-color: rgba(22, 235, 68, 0.8);
+        outline: 0;
+        outline: thin dotted \9;
+        -moz-box-shadow: 0 0 8px rgba(22, 235, 68, 0.8);
+        box-shadow: 0 0 8px rgba(22, 235, 68, 0.8) !important;
+}
 </style>
